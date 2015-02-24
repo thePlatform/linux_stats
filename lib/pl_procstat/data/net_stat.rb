79f959c235b00635104a1ca9cbf9075868f4630f
Travis Bear <travis.bear@theplatform.com>
2015-02-24 08:33:12 -0800

    Minor adjustments to .gemspec, fix some style issues
    Imported from Git
     Author: Travis Bear <travis.bear@theplatform.com> 1424795565 -0800
     Committer: Travis Bear <travis.bear@theplatform.com> 1424795565 -0800
     sha1: 1948150bea6516de9f96d7591c50e5dab19e2857
     push-state: complete
     parent-changes: bb8b103d841f95f0ac8ce36fa9f850fb278840ca=[384643]
    
    [git-p4: depot-paths = "//depot/service/ruby/pl_procstat/": change = 385115]

--- lib/pl_procstat/data/net_stat.rb
+++ lib/pl_procstat/data/net_stat.rb
@@ -7,17 +7,17 @@ class NetStat
 
   def initialize(stat_line)
 
-    #     Inter-|   Receive                                                |  Transmit
-    # face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
-    # eth0: 6330419547 9004526    0   75    0     0          0     26476 1055644570 7232244 1707    0    0 1030288    1766          0
-    # lo: 2099579347 30635436    0    0    0     0          0         0 2099579347 30635436    0    0    0     0       0          0
+#     Inter-|   Receive                                                |  Transmit
+# face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+# eth0: 6330419547 9004526    0   75    0     0          0     26476 1055644570 7232244 1707    0    0 1030288    1766          0
+# lo: 2099579347 30635436    0    0    0     0          0         0 2099579347 30635436    0    0    0     0       0          0
 
     words = stat_line.split
     @interface = words[0]
     @bytes_rx = words[1].to_i
     @bytes_tx = words[9].to_i
 
-    # TODO: errors
+    # TODO: gather metrics on tx/rx errors
     #puts("#{interface} bytes_rx: #{bytes_rx}, tx: #{bytes_tx}")
   end
 end
--- pl_procstat.gemspec
+++ pl_procstat.gemspec
@@ -13,11 +13,11 @@ Gem::Specification.new do |spec|
   spec.homepage      = ''
   spec.license       = 'thePlatform'
 
-  spec.files         = `git ls-files -z`.split("\x0")
+  spec.files         = Dir['lib/**/*.rb'] + Dir['README.md'] + Dir['docs/**/*'] - Dir['**/*~']
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.7"
-  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency 'bundler', '~> 1.7'
+  spec.add_development_dependency 'rake', '~> 10.0'
 end

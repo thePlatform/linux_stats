9a86ce807f8a90fc0368b5e0c1bd01d54edf5f6d
Travis Bear <travis.bear@theplatform.com>
2015-03-20 08:08:48 -0800

    Fix build break
    Imported from Git
     Author: Travis Bear <travis.bear@theplatform.com> 1426867608 -0700
     Committer: Travis Bear <travis.bear@theplatform.com> 1426867608 -0700
     sha1: f24447cbdf5128c9e6e79cb89828942f32e65900
     push-state: complete
     parent-changes: 3245bd347a7613bdc4483fa3ce98a469b69b106f=[389369]
    
    [git-p4: depot-paths = "//depot/service/ruby/pl_procstat/": change = 389498]

--- spec/cpu_stat_spec.rb
+++ spec/cpu_stat_spec.rb
@@ -1,19 +1,19 @@
 require 'pl_procstat'
 
-CENTOS_5_DATA = 'cpu  53054583 690 12818403 154625262 985643 2316151 7762682 0'
-CENTOS_6_DATA = 'cpu0 2533647 9153 430212 109730676 569761 18858 73181 0 0'
+CENTOS_5_CPU_DATA = 'cpu  53054583 690 12818403 154625262 985643 2316151 7762682 0'
+CENTOS_6_CPU_DATA = 'cpu0 2533647 9153 430212 109730676 569761 18858 73181 0 0'
 
 describe 'CPU Stats Class' do
 
   it 'should parse Centos 5 data' do
-    cpu_stat = CPU::Stats.new(CENTOS_5_DATA)
+    cpu_stat = CPU::Stats.new(CENTOS_5_CPU_DATA)
     expect(cpu_stat.name).to eq 'cpu'
     expect(cpu_stat.user).to eq 53054583
     expect(cpu_stat.nice).to eq 690
   end
 
   it 'should parse Centos 6 data' do
-    cpu_stat = CPU::Stats.new(CENTOS_6_DATA)
+    cpu_stat = CPU::Stats.new(CENTOS_6_CPU_DATA)
     expect(cpu_stat.name).to eq 'cpu0'
     expect(cpu_stat.user).to eq 2533647
     expect(cpu_stat.iowait).to eq 569761
--- spec/disk_stat_spec.rb
+++ spec/disk_stat_spec.rb
@@ -1,18 +1,18 @@
 require 'pl_procstat'
 
 BYTES_PER_SECTOR = 512
-CENTOS_5_DATA = '  843809   117910 19447928  3456290  6750791  9315495 128530206 31415150        0  6097640 34871410'
-CENTOS_6_DATA = ' 4569504   134368 414454495 84656111  1886305  6114130 64003426 32624791        0  5847939 117280149'
+CENTOS_5_DISK_DATA = '  843809   117910 19447928  3456290  6750791  9315495 128530206 31415150        0  6097640 34871410'
+CENTOS_6_DISK_DATA = ' 4569504   134368 414454495 84656111  1886305  6114130 64003426 32624791        0  5847939 117280149'
 
-describe 'CPU Stats Class' do
+describe 'Disk Stats Class' do
 
   it 'should parse Centos 5 data' do
-    disk_stat = BlockIO::Stats.new(CENTOS_5_DATA, BYTES_PER_SECTOR)
+    disk_stat = BlockIO::Stats.new(CENTOS_5_DISK_DATA, BYTES_PER_SECTOR)
     expect(disk_stat.reads).to eq 843809
   end
 
   it 'should parse Centos 6 data' do
-    disk_stat = BlockIO::Stats.new(CENTOS_6_DATA, BYTES_PER_SECTOR)
+    disk_stat = BlockIO::Stats.new(CENTOS_6_DISK_DATA, BYTES_PER_SECTOR)
     expect(disk_stat.reads).to eq 4569504
     expect(disk_stat.queue_time_ms).to eq 117280149
   end

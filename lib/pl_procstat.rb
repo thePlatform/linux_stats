3e6dcb373af8d89e777828e10fc7347cdfb0ef1e
George Lester <george.lester@theplatform.com>
2015-03-25 09:10:52 -0800

    Ensured that module declarations occur first in the list of requires
    Imported from Git
     Author: george.lester <george.lester@theplatform.com> 1427303442 -0700
     Committer: george.lester <george.lester@theplatform.com> 1427303446 -0700
     sha1: a9682049c96400492fda317fddb5aae0bd9e082a
     push-state: complete
     parent-changes: 24b72eb2e6457ea3a1789321328a6ed0ee36d240=[390259]
    
    [git-p4: depot-paths = "//depot/service/ruby/pl_procstat/": change = 390306]

--- lib/pl_procstat.rb
+++ lib/pl_procstat.rb
@@ -1,4 +1,4 @@
-
+require 'pl_procstat/version'
 require 'pl_procstat/data/cpu_stat'
 require 'pl_procstat/data/net_stat'
 require 'pl_procstat/data/disk_io_stat'
@@ -6,4 +6,3 @@ require 'pl_procstat/data/disk_io_stat'
 require 'pl_procstat/inspect'
 require 'pl_procstat/os_stats'
 require 'pl_procstat/process_stats'
-require 'pl_procstat/version'
